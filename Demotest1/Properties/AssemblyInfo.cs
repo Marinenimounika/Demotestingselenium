76baf18f1fda1f35b4e8e1131daaaa86191c50f2
mmarineni <mmarineni@BHG-DC2-VWKS41>
2021-06-01 04:12:17 -0400

    Add project files.

---
+++ Demotest1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Demotest1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Demotest1")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a6172ab9-abde-4958-803b-e943ad228e2a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Demotest1/UnitTest1.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+
+
+namespace Demotest1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public static void Main()        {
+
+
+            /*IWebDriver webDriverobj1 = new ChromeDriver();
+            webDriverobj1.Navigate().GoToUrl("https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin");
+
+            webDriverobj1.FindElement(By.XPath("//input[@type='email']")).SendKeys("marinenimounika109@gmail.com");
+
+            webDriverobj1.FindElement(By.XPath("//span[@class='VfPpkd-vQzf8d']")).Click();
+            webDriverobj1.FindElement(By.XPath("//input[@type='password']")).SendKeys("Lakshmidevi@1");
+            webDriverobj1.FindElement(By.XPath("//span[@class='VfPpkd-vQzf8d']")).Click();
+            webDriverobj1.Close();*/
+
+            IWebDriver webDriverobj1 = new ChromeDriver();
+            webDriverobj1.Navigate().GoToUrl("https://www.google.com/");
+
+            webDriverobj1.FindElement(By.XPath("//input[@name='q']")).SendKeys("c# tutorial");
+
+            webDriverobj1.FindElement(By.Name("btnK")).Click();
+            webDriverobj1.Close();
+
+
+
+
+
+            /*if condition:
+            webDriverobj1.FindElement(By.XPath("//div[@class='o6cuMc']"));
+            webDriverobj1.FindElement(By.XPath("//span[@class='NlWrkb snByac']")).Click();
+            webDriverobj1.FindElement(By.XPath("//div[@class='JPdR6b ILYVD qjTEB']")).Click();
+
+
+
+            webDriverobj1.FindElement(By.XPath("//input[@id='firstName']"));
+
+            webDriverobj1.FindElement(By.XPath("//input[@id='lastName']"));
+
+            webDriverobj1.FindElement(By.XPath("//input[@id='username']"));
+
+            webDriverobj1.FindElement(By.XPath("//input[@name='Passwd']"));
+
+            webDriverobj1.FindElement(By.XPath("//input[@name='ConfirmPasswd']"));
+
+            webDriverobj1.FindElement(By.XPath("//input[@class='VfPpkd-RLmnJb']")).Click();
+            webDriverobj1.FindElement(By.XPath("//input[@class='VfPpkd-RLmnJb']")).Click();
+            webDriverobj1.FindElement(By.XPath("//div[@class='VfPpkd-vQzf8d']")).Click();*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
